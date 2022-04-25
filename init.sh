984d1fc39ded3b44c7a30ac36db0f0aaacb9dd71
Eliora59 <36574784+Eliora59@users.noreply.github.com>
2022-04-25 14:13:20 +0200

    Remove SID Version for Debian

--- init.sh
+++ init.sh
@@ -6,23 +6,29 @@ ARCH='NAME="Arch Linux"'
 
 
 if [ "$DETECT_DISTRIB" == "$DEBIAN" ];then
-echo "Modification of sources.list for SID"
-echo "deb https://deb.debian.org/debian sid main contrib non-free" > /etc/apt/sources.list
-echo "deb-src https://deb.debian.org/debian sid main contrib non-free" >> /etc/apt/sources.list
-
-echo "Refresh metadatas of repository"
-apt update
-echo "Installation of prerequisite"
+echo "Exécution du playbook d'installation pour Debian"
 DEBIAN_FRONTEND=noninteractive apt install -y git ansible
-
-echo "Exécution du playbook d'installation"
 /usr/bin/ansible-pull SelfInstall.yml -U https://github.com/Eliora59/SelfInstall.git -vvv
 
 elif [ "$DETECT_DISTRIB" == "$RHEL" ];then
+echo "Exécution du playbook d'installation pour RedHat"
 dnf install -y git ansible
 /usr/bin/ansible-pull SelfInstall.yml -U https://github.com/Eliora59/SelfInstall.git -vvv
 
 elif [ "$DETECT_DISTRIB" == "$ARCH" ];then
+echo "Exécution du playbook d'installation pour ArchLinux"
 pacman -S git ansible --noconfirm
 /usr/bin/ansible-pull SelfInstall.yml -U https://github.com/Eliora59/SelfInstall.git -vvv
 fi
+
+
+
+
+
+#echo "Modification of sources.list for SID"
+#echo "deb https://deb.debian.org/debian sid main contrib non-free" > /etc/apt/sources.list
+#echo "deb-src https://deb.debian.org/debian sid main contrib non-free" >> /etc/apt/sources.list
+
+#echo "Refresh metadatas of repository"
+#apt update
+#echo "Installation of prerequisite"
